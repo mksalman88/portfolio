2f057b2b4f39c3ad1f8b479677123ceeda63d492
mksalman88 <110806827+mksalman88@users.noreply.github.com>
2025-10-03 01:47:57 +0000

    Deploying to gh-pages from @ mksalman88/my-al-folio@1e3497a032d39e5581de7bf2a2d4d98c56a4d3d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,18 +469,11 @@ ninja.data = [{
           window.open("mailto:%6D%6B%73%61%6C%6D%61%6E%70%65%72%73@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/al-folio/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://www.linkedin.com/in/maliksal", "_blank");
         },
       },{
         id: 'social-custom_social',
