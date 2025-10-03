45c352dd3d243047da281fd5121428d949b94142
mksalman88 <110806827+mksalman88@users.noreply.github.com>
2025-10-03 01:36:11 +0000

    Deploying to gh-pages from @ mksalman88/my-al-folio@354ac946a9e4b4a08e733c037bfdceb9d3369fbd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio/cv/";
-          },
         },{id: "nav-bookshelf",
           title: "bookshelf",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/books/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
