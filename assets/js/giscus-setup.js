28bb6e1a029a03b08e1e80f7507146d9315fbc90
mksalman88 <110806827+mksalman88@users.noreply.github.com>
2025-10-13 03:26:59 +0000

    Deploying to gh-pages from @ mksalman88/portfolio@1b816d3bab02b95a631c71f5866f2ec2ac97572f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mksalman88/my-al-folio",
+    "data-repo": "mksalman88/portfolio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
